c39ed0a1e5ee2642833ab7c3351926e84f4768e6
bvechiato <beaaccounts@icloud.com>
2023-11-07 15:27:22 +0000

    FlightPathTest

---
+++ FlightPathTest.java
@@ -0,0 +1,68 @@
+package uk.ac.ed.inf;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.RepeatedTest;
+import uk.ac.ed.inf.ilp.data.*;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+
+
+/*
+ * ASSUMPTIONS
+ *
+ */
+public class FlightPathTest extends TestCase {
+    public static Random RANDOM = new Random();
+    public static LngLatHandler HANDLER = new LngLatHandler();
+    public static RESTAccess REST_ACCESS = new RESTAccess();
+
+    /**
+     * Generates random points until it's in a region
+     *
+     * @param region given region
+     * @return random point in that region
+     */
+    public LngLat createLngLatInRegion(NamedRegion region) {
+        LngLat[] vertices = region.vertices();
+        Arrays.sort(vertices, Comparator
+                .comparing(LngLat::lng)
+                .thenComparing(LngLat::lat)
+        );
+        double lng;
+        double lat;
+        LngLat pos = new LngLat(0, 0);
+
+        while (!HANDLER.isInRegion(pos, region)) {
+            lng = RANDOM.nextDouble(vertices[0].lng(), vertices[3].lng());
+            lat = RANDOM.nextDouble(vertices[0].lat(), vertices[3].lat());
+
+            pos = new LngLat(lng, lat);
+        }
+
+        return pos;
+    }
+
+    @RepeatedTest(100)
+    public void testPathFromRandomPositionInNoFlyZone() throws IOException, InterruptedException {
+        NamedRegion[] noFlyZones = REST_ACCESS.getNoFlyZones(uk.ac.ed.inf.TestConstants.ENDPOINT);
+        FlightPathCalculator pathCalculator = new FlightPathCalculator(uk.ac.ed.inf.TestConstants.CENTRAL_AREA, noFlyZones);
+
+        LngLat pos = createLngLatInRegion(noFlyZones[RANDOM.nextInt(0, noFlyZones.length - 1)]);
+        List<LngLat> path = pathCalculator.findPath(pos, uk.ac.ed.inf.TestConstants.APPLETON_TOWER);
+
+
+        System.err.println("From [" + pos.lng() + ", " + pos.lat() + "]");
+        if (!path.isEmpty()) {
+            System.out.println("\tPath found: ");
+            for (LngLat point : path) {
+                System.out.println("[" + point.lng() + ", " + point.lat() + "],");
+            }
+        }
+
+        assertTrue(path.isEmpty());
+    }
+}
--- TestConstants.java
+++ TestConstants.java
@@ -1,16 +1,30 @@
 package uk.ac.ed.inf;
 
 import uk.ac.ed.inf.ilp.data.LngLat;
+import uk.ac.ed.inf.ilp.data.NamedRegion;
 import uk.ac.ed.inf.ilp.data.Pizza;
 import uk.ac.ed.inf.ilp.data.Restaurant;
 
 import java.time.DayOfWeek;
 
 public final class TestConstants {
+    public static final String ENDPOINT = "https://ilp-rest.azurewebsites.net/";
+
+    public final static LngLat APPLETON_TOWER = new LngLat(-3.186874, 55.944494);
+
+    public static final NamedRegion CENTRAL_AREA = new NamedRegion("Central Area", new LngLat[] {
+            new LngLat(-3.190578818321228,55.94402412577528),
+            new LngLat(-3.1899887323379517,55.94284650540911),
+            new LngLat(-3.187097311019897,55.94328811724263),
+            new LngLat(-3.187682032585144, 55.944477740393744),
+            new LngLat(-3.190578818321228,55.94402412577528)
+    }
+    );
+
     public static final Restaurant[] RESTAURANTS = new Restaurant[] {
             new Restaurant("Civerinos Slice",
                     new LngLat(-3.1912869215011597, 55.945535152517735),
-                    new DayOfWeek[] {DayOfWeek.MONDAY, DayOfWeek.SUNDAY},
+                    new DayOfWeek[] {DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.FRIDAY, DayOfWeek.SATURDAY, DayOfWeek.SUNDAY},
                     new Pizza[] {
                             new Pizza("Margarita", 1000),
                             new Pizza("Calzone", 1400)
@@ -18,7 +32,7 @@ public final class TestConstants {
             ),
             new Restaurant("Sora Lella Vegan Restaurant",
                     new LngLat(-3.202541470527649, 55.943284737579376),
-                    new DayOfWeek[] {DayOfWeek.MONDAY, DayOfWeek.FRIDAY},
+                    new DayOfWeek[] {DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY, DayOfWeek.FRIDAY},
                     new Pizza[] {
                             new Pizza("Meat Lover", 1400),
                             new Pizza("Vegan Delight", 1100)
@@ -26,7 +40,7 @@ public final class TestConstants {
             ),
             new Restaurant("Domino's Pizza - Edinburgh - Southside",
                     new LngLat(-3.1838572025299072, 55.94449876875712),
-                    new DayOfWeek[] {DayOfWeek.WEDNESDAY, DayOfWeek.SUNDAY},
+                    new DayOfWeek[] {DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY, DayOfWeek.FRIDAY, DayOfWeek.SATURDAY, DayOfWeek.SUNDAY},
                     new Pizza[] {
                             new Pizza("Super Cheese", 1400),
                             new Pizza("All Shrooms", 900)
@@ -34,7 +48,7 @@ public final class TestConstants {
             ),
             new Restaurant("Sodeberg Pavillion",
                     new LngLat(-3.1940174102783203, 55.94390696616939),
-                    new DayOfWeek[] {DayOfWeek.TUESDAY, DayOfWeek.SUNDAY},
+                    new DayOfWeek[] {DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.SATURDAY, DayOfWeek.SUNDAY},
                     new Pizza[] {
                             new Pizza("Proper Pizza", 1400),
                             new Pizza("Pineapple & Ham & Cheese", 900)
